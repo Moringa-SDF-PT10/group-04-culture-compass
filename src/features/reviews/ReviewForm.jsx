a07cd53f1cc16d73a63a9f644ef6070f1128c381
Ruth Siyoi <ruth.siyoi@gmail.com>
2025-05-28 10:23:14 +0300

    adds review form

---
+++ src/features/reviews/ReviewForm.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import { Star } from 'lucide-react';
+
+export default function ReviewForm({ countryId, countryName, onSubmit}) {
+
+  const [reviewTitle,setReviewTitle]= useState('');
+  const [comment, setComment] = useState('');
+  const [rating, setRating] = useState(0);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (rating === 0) return;
+    onSubmit(countryId, { rating: Number(rating), comment });
+    setComment('');
+    setRating(0);
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="reviewForm">
+      <h3 className="title">Add Review for {countryName}</h3>
+      <div className="rating-content">
+        <label className="rating">Rating:</label>
+        {[1, 2, 3, 4, 5].map((num) => (
+          <Star
+            key={num}
+            onClick={() => setRating(num)}
+            className={`${rating >= num ? 'text-yellow-400' : 'text-gray-300'}`}
+            fill={rating >= num ? 'currentColor' : 'none'}
+            required
+          />
+          
+        ))}
+        
+      </div>
+      <div className="review-content">
+        <label>Comment: </label>
+        <textarea
+          value={comment}
+          onChange={(e) => setComment(e.target.value)}
+          className="comment"
+          placeholder='Please Review me....'
+          required
+        ></textarea>
+      </div>
+      
+      <button type="submit" className="submit">Review</button>
+    </form>
+  );
+}
