9a257ab87b398cfe9be8a9a892cfb9cecde77135
Arnold <arnold.mutama@gmail.com>
2025-05-30 16:38:16 +0300

    Adds a SignUp Form

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import LoginForm from './features/authentication/LoginForm'
+import SignUpForm from './features/authentication/SignUpForm'
 
 
 function App() {
@@ -25,9 +26,7 @@ function App() {
   ]);
 
   return (
-    <>
-      <LoginForm users={users} />
-    </>
+    
   )
 }
 
