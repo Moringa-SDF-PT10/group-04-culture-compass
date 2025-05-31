326605e8867ce8b056faa5efb9e26f50905d46f5
michael <aoinihonjin@gmail.com>
2025-05-31 14:05:28 +0300

    added a feature for visually imapred users aria features for screen readers

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -23,6 +23,43 @@ import { Link } from 'react-router-dom';
               className="brand-logo"
             />
           </Link>
+
+            {/*desktop navigation links*/}
+
+          <div className='navbar-links'>
+            <Link to="/" className="nav-link">Home</Link>
+
+            {/*If logged in these will show too */}
+            {isLoggedIn && (<>
+            <link to = '/countries' className='nav-link'>countries</link>
+            <link to = '/countries' className='nav-link'>countries</link>
+            <link to = '/countries' className='nav-link'>countries</link>
+            </>
+            )}
+          </div>
+
+          {/*user actionslike logging in */}
+          <div className='navbar-actions'>
+            {!isLoggedIn ?(
+                <div className='auth-buttons'>
+                    <button onClick={handleLogin} className ='button-secondary-small'>
+                        Login
+                    </button>
+                    <link to='signup' className='button-primary-small'>
+                    signup
+                    </link>
+
+                </div>
+
+            ) : (
+                <div className='user-menu'>
+                    <button className='user-profile-btn' onClick={toggleUserDropdown} aria-expanded={showUserDropdown} aria-haspopup={true}>
+                        
+                    </button>
+                </div>
+
+            )}
+          </div>
             </div>
         </div>
     </nav>
