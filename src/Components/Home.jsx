8672407a2fd90f4acb2a0d69cac72a54a11d7651
michael <aoinihonjin@gmail.com>
2025-05-31 16:59:06 +0300

    did some basic routing that will arnold to make a routing to protected route incase user isnt logged in

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,6 +1,7 @@
 
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './index.css'
 export default function Home(){
     const Home = () => {
         return (
--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,1 +1,50 @@
-g
+import React from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import Home from '../components/Components/Home';
+import Countries from '../components/Components/Countries';
+import CountriesDetails from '../components/Components/CountriesDetails';
+import LoginForm from '../components/features/authentication/LoginForm';
+import SignUpForm from '../components/features/authentication/SignUpForm';
+import ReviewForm from '../components/features/reviews/ReviewForm';
+import ReviewCard from '../components/features/reviews/ReviewCard';
+import ProtectedRoute from '../components/common/ProtectedRoute';
+
+const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/countries" element={<Countries />} />
+      <Route path="/countries/:id" element={<CountriesDetails />} />
+
+      {/* Auth */}
+      <Route path="/login" element={<LoginForm />} />
+      <Route path="/signup" element={<SignUpForm />} />
+
+      {/* hizi ndo Protected Routes */}
+      <Route
+        path="/reviews"
+        element={
+          <ProtectedRoute>
+            <ReviewCard />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/reviews/new"
+        element={
+          <ProtectedRoute>
+            <ReviewForm />
+          </ProtectedRoute>
+        }
+      />
+
+      {/* Catch-all route */}
+      <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
+  );
+};
+
+export default AppRoutes;
+
+
+
