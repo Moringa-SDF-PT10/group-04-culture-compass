23acb6a455b8f0f2e77dc8838a4924fe0f970837
michael <aoinihonjin@gmail.com>
2025-06-01 12:50:29 +0300

    restructured the mainjsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import AppRoutes from './routes/AppRoutes';
+
+function App() {
+  return (
+   
+    <BrowserRouter>
+      <div className="app">
+        
+        <Navbar />
+        <main style={{ flex: 1, minHeight: 'calc(100vh - 140px)' }}>
+          <AppRoutes />
+        </main>
+        <Footer />
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
